782683f16ef2c29f156832365901af24f861e0aa
VPAbraham <vpa456@gmail.com>
2019-07-08 17:30:27 -0600

    Change all quotations in js.main to single quotes

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-var userMin = document.querySelector("#user-min");
-var userMax = document.querySelector("#user-max");
-var minDisplay = document.querySelector("#min-display");
-var maxDisplay = document.querySelector("#max-display");
-var conflictError = document.querySelector("#conflict-error");
-var updateBtn = document.querySelector("#update-btn");
-var submitBtn = document.querySelector("#submit-btn")
-var resetBtn = document.querySelector("#reset-btn");
-var clearBtn = document.querySelector("#clear-btn");
+var userMin = document.querySelector('#user-min');
+var userMax = document.querySelector('#user-max');
+var minDisplay = document.querySelector('#min-display');
+var maxDisplay = document.querySelector('#max-display');
+var conflictError = document.querySelector('#conflict-error');
+var updateBtn = document.querySelector('#update-btn');
+var submitBtn = document.querySelector('#submit-btn')
+var resetBtn = document.querySelector('#reset-btn');
+var clearBtn = document.querySelector('#clear-btn');
 var playerOneName = document.querySelector('#player-one-name');
 var playerTwoName = document.querySelector('#player-two-name');
-var playerOneGuess = document.querySelector("#player-one-guess");
-var playerTwoGuess = document.querySelector("#player-two-guess");
+var playerOneGuess = document.querySelector('#player-one-guess');
+var playerTwoGuess = document.querySelector('#player-two-guess');
 var guessOne = document.querySelector('#player1-guess');
 var guessTwo = document.querySelector('#player2-guess');
 var challengerOne = document.querySelector('#player-one');
@@ -18,16 +18,16 @@ var challengerTwo = document.querySelector('#player-two');
 var leftSuggestion = document.querySelector('#left-tip');
 var rightSuggestion = document.querySelector('#right-tip');
 
-var leftSide = document.querySelector(".left-pane");
+var leftSide = document.querySelector('.left-pane');
 var rightSide = document.querySelector('.right-pane')
 
 // Event Listeners
 
 // leftSide.addEventListener('click', runAll)
-updateBtn.addEventListener("click", updateRange);
+updateBtn.addEventListener('click', updateRange);
 clearBtn.addEventListener('click', clearFields);
 submitBtn.addEventListener('click', submitFunc)
-leftSide.addEventListener("keyup", disableBtns);
+leftSide.addEventListener('keyup', disableBtns);
 conflictError.addEventListener('click', guessesError);
 
 // Functions
@@ -51,15 +51,15 @@ function submitFunc(e) {
   guessesError();
 }
 
-function updateRange() {
+// function updateRange() {
+
+//   minDisplay.innerHTML = userMin.value;
+//   maxDisplay.innerHTML = userMax.value;
+//   useRange(userMin.value, userMax.value);
+//   userMin.value = '';
+//   userMax.value = '';
   
-  minDisplay.innerHTML = userMin.value;
-  maxDisplay.innerHTML = userMax.value;
-  useRange(userMin.value, userMax.value);
-  userMin.value = "";
-  userMax.value = "";
-  
-}
+// }
 
 function updateRange() {
   guessesError();
@@ -70,8 +70,8 @@ function updateRange() {
     minDisplay.innerHTML = userMin.value;
     maxDisplay.innerHTML = userMax.value;
     useRange(userMin.value, userMax.value);
-    userMin.value = "";
-    userMax.value = "";
+    userMin.value = '';
+    userMax.value = '';
     displayFeedback(playerOneGuess.value, leftSuggestion);
     displayFeedback(playerTwoGuess.value, rightSuggestion);
     appendArticle();
@@ -142,17 +142,17 @@ function displayFeedback(num, element) {
 
 function appendArticle() {
 if (rightSuggestion.innerText || leftSuggestion.innerText === 'BOOM!') {
-    rightSide.insertAdjacentHTML('afterbegin', `<article class="winner">
-        <header class="winner__header">
+    rightSide.insertAdjacentHTML('afterbegin', `<article class='winner'>
+        <header class='winner__header'>
             <h4>CHALLENGER 1</h4>
             <p>VS</p>
             <h4>CHALLENGER 2 NAME</h4>
         </header>
-        <h2 class="winner__name">CHALLENGER 1 NAME</h2>
-        <h3 class="winner__status">WINNER</h3>
+        <h2 class='winner__name'>CHALLENGER 1 NAME</h2>
+        <h3 class='winner__status'>WINNER</h3>
         <footer>
-          <p class="winner__p"><span class="winner__span">2</span> GUESSES</p>
-          <p class="winner__p"><span class="winner__span">23</span> MINUTES</p>
+          <p class='winner__p'><span class='winner__span'>2</span> GUESSES</p>
+          <p class='winner__p'><span class='winner__span'>23</span> MINUTES</p>
           <button>X</button>
         </footer>
         </header>`)
